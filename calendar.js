184cd7340b52d06fdac90d22a01e6ba6dadceb19
EduardoVega731 <vegae2016@gmail.com>
2022-03-10 23:29:01 -0600

    added calendar.js and css

---
+++ calendar.js
@@ -0,0 +1,32 @@
+// months array
+const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", 
+    "December"
+];
+
+// https://www.w3schools.com/js/js_date_methods.asp
+
+const dte = new Date();
+// document.write("The current month is " + monthNames[d.getMonth()]);
+
+const day = dte.getDate();
+const month = dte.getMonth();
+const year = dte.getFullYear();
+
+//https://www.w3resource.com/javascript-exercises/javascript-date-exercise-9.php
+// Finds number of days in current month
+const currentMonthDays = new Date(year, month + 1, 0).getDate();
+
+// displays current month and year
+function setMonthYear() {
+    console.log(currentMonthDays);
+
+    currentMonth = document.getElementById("month");
+    currentMonth.innerHTML = monthNames[month];
+
+    currentYear = document.getElementById("year");
+    currentYear.innerHTML = year;
+}
+
+this.document.onload = setMonthYear();
